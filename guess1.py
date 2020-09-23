dd764aed9a7280f30bf9c7303735d5b0101fa0d7
ming1208 <weng022768@gmail.com>
2020-09-23 16:01:09 +0800

    final version

--- guess1.py
+++ guess1.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+start = input('請輸入隨機數字範圍的起始直: ')
+end = input('請輸入隨機數字範圍的結束直: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1

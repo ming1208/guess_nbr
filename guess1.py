d1dddb8ae549582ebb69623570ec2f617ad1250e
ming1208 <weng022768@gmail.com>
2020-09-23 15:50:37 +0800

    Add count function

--- guess1.py
+++ guess1.py
@@ -1,12 +1,15 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1
 	nbr = input('請猜個數字: ')
 	nbr = int(nbr)
 	if nbr == r:
-		print('恭喜!你猜中了!')
+		print('恭喜!你在第', count, '次猜中了!')
 		break
 	elif nbr > r:
 		print('比答案大!再猜小一點!')
 	elif nbr < r:
 		print('比答案小!再猜大一點!')
+	print('這是你猜的第', count, '次了!')
